980c357e48da8ff297df92eee1b85ac3b8a5332e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-01-13 21:09:25 -0600

    Added fixtures for multiples server, added toml config and parsing

--- tests/test_backups.py
+++ tests/test_backups.py
@@ -1,41 +1,43 @@
-import unittest
+import toml
 import datetime
 
 import httpx
+import pytest
 from httpx import Client
 
+devices_dict = toml.load("/config.toml")
 
-class BackupsTest(unittest.TestCase):
-    api_key = "CHANGE THIS"
 
-    ip_address = "CHANGE THIS"
-
+@pytest.mark.parametrize("devices", devices_dict.items())
+class TestBackups:
     port = 8384
 
     client: Client
 
     outdated_time = (datetime.datetime.now() - datetime.timedelta(hours=12)).timestamp()
 
-    def setUp(self):
+    @pytest.fixture(scope='function', autouse=True)
+    def setup_method(self, devices):
+        self.host = devices[0]
+        self.api_key = devices[1]
         headers = {"Authorization": f"Bearer {self.api_key}"}
         self.client = httpx.Client(headers=headers)
 
     def test_health_check(self):
-        url = f"http://{self.ip_address}:{self.port}/rest/noauth/health"
+        url = f"{self.host}/rest/noauth/health"
 
         ok = {"status": "OK"}
 
         with self.client as c:
             res = c.get(url)
-            self.assertEqual(res.json(), ok, f"Health check failed for {self.ip_address}")
+            assert res.json() == ok, f"Health check failed for {self.host}"
 
     def test_status(self):
-        url = f"http://{self.ip_address}:{self.port}/rest/stats/folder"
+        url = f"{self.host}/rest/stats/folder"
         with self.client as c:
             res = c.get(url)
             folders = dict(res.json())
             for (folder, data) in folders.items():
                 last_scan = datetime.datetime.fromisoformat(data['lastScan']).timestamp()
 
-                self.assertGreaterEqual(last_scan, self.outdated_time, f"{folder} is out of sync on {self.ip_address}")
-
+                assert last_scan >= self.outdated_time, f"{folder} is out of sync on {self.ip_address}"
