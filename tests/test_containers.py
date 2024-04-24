74fbd0eab05ebce62ceba37292990b0b67b99d6e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-04-23 20:33:24 -0500

    Condensed to one file

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 import toml
-
-from helper import get_docker_image_from_file, start_container
+import yaml
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_container_is_ready
 
 containers = toml.load("resources/config.toml")['Docker']['files'].split(',')
 
@@ -26,3 +29,32 @@ class TestContainers:
                 start_container(image)
             except Exception as e:
                 assert False, e
+
+
+def get_docker_image_from_file(path: str) -> str | None:
+    if not os.path.isfile(path):
+        raise Exception(f"Unable to find file: {path}")
+
+    with open(path) as f:
+        data: list[dict] = [yaml.load(f, Loader=yaml.SafeLoader)]
+        while data:
+            d = data.pop()
+            if 'image' in d:
+                return d['image']
+            for k, v in d.items():
+                if isinstance(v, dict):
+                    data.append(v)
+
+    raise Exception(f"Unable to find image in file: {path}")
+
+
+def start_container(image: str):
+    if image is None:
+        raise Exception("Image can't be none")
+
+    try:
+        with DockerContainer(image) as container:
+            wait_container_is_ready()
+            return container
+    except Exception as e:
+        raise e
