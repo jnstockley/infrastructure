3ae17b8f7893d239268dcf3bff6715be177c1cf6
Jack Stockley <jack@jstockley.com>
2024-07-26 12:13:14 -0500

    Updating DB User

--- docker/photo-server/immich/restore.sh
+++ docker/photo-server/immich/restore.sh
@@ -10,7 +10,7 @@ if bzip2 -t "$INPUT_FILE" > /dev/null 2>&1; then
     echo "Input file is valid"
     DECOMPRESSED_FILE=$(bzip2 -dkfc "$INPUT_FILE" >> restore.sql)
     echo "$DECOMPRESSED_FILE"
-    docker exec $CONTAINER_NAME psql -c -U jackstockley < restore.sql
+    docker exec $CONTAINER_NAME psql -c -U postgres < restore.sql
     exit 0
 else
     echo "Input file is not valid"
