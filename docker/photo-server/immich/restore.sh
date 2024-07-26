cbc10c5eaf522f34bee5a22a1eab57ef7377e507
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-07-26 11:50:45 -0500

    Added backup and restore scripts

---
+++ docker/photo-server/immich/restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the name of the Docker container
+CONTAINER_NAME="immich_postgres"
+
+
+INPUT_FILE=$1
+
+if bzip2 -t "$INPUT_FILE" > /dev/null 2>&1; then
+    echo "Input file is valid"
+    DECOMPRESSED_FILE=$(bzip2 -dkfc "$INPUT_FILE" >> restore.sql)
+    echo "$DECOMPRESSED_FILE"
+    docker exec $CONTAINER_NAME psql -c -U jackstockley < restore.sql
+    exit 0
+else
+    echo "Input file is not valid"
+    exit 1
+fi
