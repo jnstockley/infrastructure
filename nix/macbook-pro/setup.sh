c7b4e05318be023a6385a5f74f3da331b08a33bd
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-16 17:32:21 -0600

    Fix linting

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -6,7 +6,8 @@ sh <(curl -L https://nixos.org/nix/install)
 
 nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
 
-darwin-rebuild switch
-
 mkdir -p ~/.config/nix
 
+curl -o ~/.config/nix/flake.nix https://raw.githubusercontent.com/jnstockley/infrastructure/refs/heads/main/nix/macbook-pro/flake.nix
+
+darwin-rebuild switch
