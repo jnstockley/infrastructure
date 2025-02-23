e10b35b5c05181ef425577e4962583cde2faf85c
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 14:41:16 -0600

    Point to directory instead of file, cleanup

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -18,18 +18,9 @@ sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 # Source the Nix environment in the current shell
 . /nix/var/nix/profiles/default/etc/profile\.d/nix-daemon\.sh
 
-# Test the Nix installation
-nix-shell -p neofetch --run neofetch
-
-cd ~/.config/nix || exit 1
-
-#nix flake init -t nix-darwin --extra-experimental-features "nix-command flakes"
-
-#rm -rf ~/.config/nix/flake.nix
-
 ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
 
-nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix/flake.nix#macbook-pro
+nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix#macbook-pro
 
 #nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
 darwin-rebuild switch --flake ~/.config/nix#macbook-pro
