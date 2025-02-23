14317644981e3c916005877488c84d01ffcd086b
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 15:40:31 -0600

    Testing installing nix os manually

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -15,10 +15,12 @@ git clone https://github.com/jnstockley/infrastructure.git ~/Documents/GitHub/In
 ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
 
 # Download and install Nix
-#sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
 # Source the Nix environment in the current shell
-#. /nix/var/nix/profiles/default/etc/profile\.d/nix-daemon\.sh
+. /nix/var/nix/profiles/default/etc/profile\.d/nix-daemon\.sh
+
+# nix flake init -t nix-darwin/master --extra-experimental-features "nix-command flakes"
 
 nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix#macbook-pro
 
