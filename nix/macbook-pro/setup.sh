bc0a83d8de8ed1a2aff0b347b276e6117f132f39
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 14:21:58 -0600

    Fixing script

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -12,13 +12,22 @@ mkdir -p ~/.config/nix
 
 git clone https://github.com/jnstockley/infrastructure.git ~/Documents/GitHub/Infrastructure/
 
-ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
-
+# Download and install Nix
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
 # Source the Nix environment in the current shell
 . /nix/var/nix/profiles/default/etc/profile\.d/nix-daemon\.sh
 
-nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
+# Test the Nix installation
+nix-shell -p neofetch --run neofetch
+
+cd ~/.config/nix || exit 1
+
+#nix flake init -t nix-darwin --extra-experimental-features "nix-command flakes"
+
+ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
 
 nix run nix-darwwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix/flake.nix#macbook-pro
+
+#nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
+darwin-rebuild switch --flake ~/.config/nix#macbook-pro
