38f105c1655a829108c160a7d2766cbd8f94ba22
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 19:38:06 -0600

    Speed up install, re-add --impure flag

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -4,8 +4,13 @@ sudo xcode-select --install &>/dev/null
 
 # Download and install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
-    sh -s -- install
+    sudo sh -s -- install
 
+# Source the Nix environment in the current shell
+# shellcheck disable=SC1091
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+echo "Waiting for xcode-select to complete..."
 until xcode-select --print-path &>/dev/null; do
     sleep 5
 done
@@ -19,10 +24,6 @@ git clone https://github.com/jnstockley/infrastructure.git ~/Documents/GitHub/In
 ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
 
 #sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-
-# Source the Nix environment in the current shell
-# shellcheck disable=SC1091
-. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 # nix flake init -t nix-darwin/master --extra-experimental-features "nix-command flakes"
 
