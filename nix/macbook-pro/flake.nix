c0c7450f309b2dec117d397c4f8a64dac30b7eb5
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 21:00:41 -0600

    Disable for testing

--- nix/macbook-pro/flake.nix
+++ nix/macbook-pro/flake.nix
@@ -61,16 +61,16 @@
               "docker"
               "roblox"
             ];
-            masApps = {
-              "Bitwarden" = 1352778147;
-              "Hidden Bar" = 1452453066;
-              "Windows App" = 1295203466;
-              "Wireguard" = 1441195209;
-              "Excel" = 62058435;
-              "Powerpoint" = 462062816;
-              "Word" = 462054704;
-              "OneDrive" = 823766827;
-            };
+            #masApps = {
+            #  "Bitwarden" = 1352778147;
+            #  "Hidden Bar" = 1452453066;
+            #  "Windows App" = 1295203466;
+            #  "Wireguard" = 1441195209;
+            #  "Excel" = 62058435;
+            #  "Powerpoint" = 462062816;
+            #  "Word" = 462054704;
+            #  "OneDrive" = 823766827;
+            #};
             onActivation.cleanup = "zap";
           };
 
