c81351289b74a8567633b50e89e6a2f5e2d7d241
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-23 19:29:16 -0600

    Speed up install, re-add --impure flag

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -2,6 +2,10 @@
 
 sudo xcode-select --install &>/dev/null
 
+# Download and install Nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
+    sh -s -- install
+
 until xcode-select --print-path &>/dev/null; do
     sleep 5
 done
@@ -14,17 +18,15 @@ git clone https://github.com/jnstockley/infrastructure.git ~/Documents/GitHub/In
 
 ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
 
-# Download and install Nix
 #sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
-  sh -s -- install
 
 # Source the Nix environment in the current shell
+# shellcheck disable=SC1091
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 # nix flake init -t nix-darwin/master --extra-experimental-features "nix-command flakes"
 
-nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix#macbook # --impure
+nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix#macbook --impure
 
 #nix --extra-experimental-features "nix-command flakes" run nix-darwin/master#darwin-rebuild -- switch
 darwin-rebuild switch --flake ~/.config/nix#macbook
