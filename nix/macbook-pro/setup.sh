cda01974aad4d02ed7d27beb2616902d61041070
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-02-26 19:34:08 -0600

    Uncommenting masApps, adding exceptions for GitHub actions, and macOS vm

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -29,12 +29,16 @@ mkdir -p ~/.config/nix
 # Check if GITHUB_ACTION is set and doesn't equals 1
 if [ "${GITHUB_ACTION}" != "1" ]; then
     git clone https://github.com/jnstockley/infrastructure.git ~/Documents/GitHub/Infrastructure/
+    if system_profiler SPHardwareDataType | grep -q "VMware\|VirtualBox\|Parallels\|QEMU\|Hypervisor"; then
+        sed -i '' '/masApps = {/,/};/d' ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix
+    fi
 else
     rm -rf ~/Documents/GitHub/Infrastructure/
     ln -s "$GITHUB_WORKSPACE" ~/Documents/GitHub
     # create file with content
     echo "access-tokens = github.com=${GITHUB_TOKEN}" >~/.config/nix/nix.conf
     sed -i '' 's/jackstockley/'"$(whoami)"'/g' ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix
+    sed -i '' '/masApps = {/,/};/d' ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix
 fi
 
 ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/flake.nix ~/.config/nix/flake.nix
