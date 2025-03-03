22bb8a0afd594d65cb83d539d578ab7bd7b8b5a6
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-02 20:22:15 -0600

    Setup ohMyZSH

---
+++ nix/macbook-pro/zsh.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  enable = true;
+  history.size = 10000;
+  history.path = "${config.xdg.dataHome}/zsh/history";
+  oh-my-zsh = {
+    enable = true;
+    plugins = [
+      "git"
+      "docker"
+    ];
+  };
+  plugins = [
+    #{
+    # will source zsh-autosuggestions.plugin.zsh
+    #name = "zsh-autosuggestions";
+    #src = pkgs.zsh-autosuggestions;
+    #file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+    #}
+    {
+      name = "zsh-completions";
+      src = pkgs.zsh-completions;
+      file = "share/zsh-completions/zsh-completions.zsh";
+    }
+    {
+      name = "zsh-syntax-highlighting";
+      src = pkgs.zsh-syntax-highlighting;
+      file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+    }
+  ];
+}
