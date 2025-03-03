94ec997e945b8daabaa0c8b4383456cf95e2bbd8
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-02 20:04:29 -0600

    Fix follows

--- nix/macbook-pro/flake.nix
+++ nix/macbook-pro/flake.nix
@@ -9,7 +9,7 @@
 
     # Home Manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
