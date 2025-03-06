3c5e26b49eadc538853ea94802ada56572065d72
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-05 19:44:57 -0600

    Add completions

--- nix/macbook-pro/zsh.nix
+++ nix/macbook-pro/zsh.nix
@@ -53,6 +53,10 @@
     export NVM_DIR="$HOME/.nvm"
     [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
     [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+    # uv
+    eval "$(uv generate-shell-completion zsh)"
+    eval "$(uvx --generate-shell-completion zsh)"
   '';
   oh-my-zsh = {
     enable = true;
