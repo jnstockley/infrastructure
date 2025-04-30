187a3aa4a8209333529ec28fc9f3062f633b239e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-04-29 19:34:43 -0500

    Update home manager version

--- nix/macbook-pro/flake.nix
+++ nix/macbook-pro/flake.nix
@@ -8,7 +8,7 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
 
     # Home Manager
-    home-manager.url = "github:nix-community/home-manager/release-master";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
