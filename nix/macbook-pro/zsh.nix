e6be0b9640cedb3ca91db87107f680c9a8898f72
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-04-29 19:37:44 -0500

    Switch to initContent

--- nix/macbook-pro/zsh.nix
+++ nix/macbook-pro/zsh.nix
@@ -34,7 +34,7 @@
     [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
     [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
   '';
-  initExtra = ''
+  initContent = ''
     export PATH="/Users/jackstockley/.local/bin:$PATH"
 
     export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
