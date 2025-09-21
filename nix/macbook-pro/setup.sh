58491cac8f7df7cedc7a1f0cf8e9e5b4868b90f6
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-09-21 13:17:04 -0500

    Add logging

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -25,6 +25,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ -n "$GITHUB_TOKEN" ]; then
   export NIX_CONFIG="access-tokens = github.com=${GITHUB_TOKEN}"
   . "$SCRIPT_DIR/../scripts/mas-disable.sh" "$SCRIPT_DIR/flake.nix"
+  echo "$SCRIPT_DIR"
   ls -s "$SCRIPT_DIR" ~/.config/
 else # Not run in GitHub Actions
   if [ ! -d ~/Documents/GitHub/Infrastructure/.git ]; then
