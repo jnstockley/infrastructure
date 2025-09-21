14f535bd6c34736de0c58eccf0fa607ea6547f61
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-09-21 12:57:05 -0500

    Remove sudo

--- nix/macbook-pro/setup.sh
+++ nix/macbook-pro/setup.sh
@@ -95,6 +95,6 @@ ln -s ~/Documents/GitHub/Infrastructure/nix/macbook-pro/ ~/.config/
 
 #sudo chmod -R 755 /etc/nix/
 
-sudo nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/macbook-pro#macbook --impure
+nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/macbook-pro#macbook --impure
 
 exec "$SHELL"
