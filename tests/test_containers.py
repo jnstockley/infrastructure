6db25c38677c1b20a4cceedc55baeb7ae5347d48
Jack Stockley <jack@jstockley.com>
2026-07-23 19:17:17 -0500

    Fix ruff check errors

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -3,6 +3,7 @@ import os
 import pytest
 import yaml
 from testcontainers.core.container import DockerContainer
+
 from . import logger
 
 containers = os.environ["FILES"].split(",")
@@ -14,9 +15,7 @@ class TestContainers:
     def setup_method(self, container: str):
         self.path = None
         if container.endswith(".yml") and (
-            container.startswith("docker/racknerd/")
-            or container.startswith("docker/photo-server/")
-        ):
+            container.startswith(("docker/racknerd/", "docker/photo-server/"))):
             self.path = container
 
     def test_container(self, container: DockerContainer):
@@ -24,7 +23,7 @@ class TestContainers:
             try:
                 logger.info(f"Starting container from file: {self.path}")
                 image = get_docker_image_from_file(self.path)
-            except Exception as e:
+            except DockerException as e:
                 assert False, f"Unable to get docker image from file: {self.path}, {e}"
 
             if not image:
@@ -32,14 +31,14 @@ class TestContainers:
 
             try:
                 start_container(image)
-            except Exception as e:
+            except DockerException as e:
                 assert False, f"Unable to start container: {image}, {e}"
 
 
 def get_docker_image_from_file(path: str) -> str | None:
     if not os.path.isfile(path):
         logger.error(f"Unable to find file: {path}")
-        raise Exception(f"Unable to find file: {path}")
+        raise DockerException(f"Unable to find file: {path}")
 
     with open(path) as f:
         data: list[dict] = [yaml.load(f, Loader=yaml.SafeLoader)]
@@ -47,22 +46,25 @@ def get_docker_image_from_file(path: str) -> str | None:
             d = data.pop()
             if "image" in d:
                 return d["image"]
-            for k, v in d.items():
+            for k, v in d.values():
                 if isinstance(v, dict):
                     data.append(v)
 
     logger.error(f"Unable to find image in file: {path}")
-    raise Exception(f"Unable to find image in file: {path}")
+    raise DockerException(f"Unable to find image in file: {path}")
 
 
 def start_container(image: str):
     if image is None:
         logger.error("Image can't be none")
-        raise Exception("Image can't be none")
+        raise DockerException("Image can't be none")
 
     try:
         with DockerContainer(image) as container:
             return container
     except Exception as e:
         logger.error(f"Error starting container, {e}")
-        raise e
+        raise
+
+class DockerException(Exception):
+    pass
